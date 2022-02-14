994f9bddd89d770ce02a4f8dc06cfa37684e2104
zhangjunwei <3128414854@qq.com>
2022-02-14 10:15:06 +0800

    Spring-aop

---
+++ src/main/java/com/lear/BeforeAndAfterCon.java
@@ -0,0 +1,21 @@
+package com.lear;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : zjw
+ * @date : 9:42 2022/2/14
+ */
+
+@RestController
+@RequestMapping("/bfAop")
+public class BeforeAndAfterCon {
+    @GetMapping("/{val}")
+    public String testAop(@PathVariable Integer val) {
+        System.out.println(1/val);
+        return "Hello " + val;
+    }
+}
